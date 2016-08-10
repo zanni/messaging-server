22986fc3306084a6e4a22eeb7053440502f49a3a
Bertrand Zanni <zanni.bertrand@gmail.com>
2016-08-11 00:03:02 +0200

    fix test

--- backend-rabbitmq/src/test/java/com/bzanni/messagingserver/ApplicationUnitTest.java
+++ backend-rabbitmq/src/test/java/com/bzanni/messagingserver/ApplicationUnitTest.java
@@ -4,19 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.PropertySource;
 
 import com.bzanni.messagingserver.config.RabbitMQConfig;
 import com.bzanni.messagingserver.config.RabbitmqQueueEventConfig;
+import com.bzanni.messagingserver.controller.MessagingServerController;
+import com.bzanni.messagingserver.controller.RabbitMQAuthController;
 import com.bzanni.messagingserver.controller.RabbitmqQueueEventListener;
+import com.bzanni.messagingserver.service.ActiveSessionService;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.bzanni.messagingserver" }, excludeFilters = {
 		@ComponentScan.Filter(value = RabbitMQConfig.class, type = FilterType.ASSIGNABLE_TYPE),
 		@ComponentScan.Filter(value = RabbitmqQueueEventConfig.class, type = FilterType.ASSIGNABLE_TYPE),
 		@ComponentScan.Filter(value = RabbitmqQueueEventListener.class, type = FilterType.ASSIGNABLE_TYPE),
+		@ComponentScan.Filter(value = RabbitMQAuthController.class, type = FilterType.ASSIGNABLE_TYPE),
+		@ComponentScan.Filter(value = ActiveSessionService.class, type = FilterType.ASSIGNABLE_TYPE),
+		@ComponentScan.Filter(value = MessagingServerController.class, type = FilterType.ASSIGNABLE_TYPE),
 		@ComponentScan.Filter(value = Application.class, type = FilterType.ASSIGNABLE_TYPE) })
-@PropertySource({ "file:${messagingserver.home}/application.properties" })
 public class ApplicationUnitTest {
 
 	public ApplicationUnitTest() {
